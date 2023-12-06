3a69bac62b56422d3900c171c0916e7fa8967af2
Ibrahima <114165898+Cordapeche@users.noreply.github.com>
2023-12-06 11:08:16 +0100

    big update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    $(document).ready(function(){
+        $(".contenloader").slice(0, 4).show();
+        $("#loadMore").on("click", function(e){
+          e.preventDefault();
+          $(".contenloader:hidden").slice(0, 4).slideDown();
+          if($(".contenloader:hidden").length == 0) {
+            $("#loadMore").text("").addClass("noContent");
+          }
+        });
+        
+      })
+      
     $(".fancybox").fancybox({
-          openEffect: "none",
-          closeEffect: "none"
-      });
-      
-      $(".zoom").hover(function(){
-          
-          $(this).addClass('transition');
-      }, function(){
-          
-          $(this).removeClass('transition');
-      });
-  });
-      
+        openEffect: "none",
+        closeEffect: "none"
+    });
+
+    $(".zoom").hover(function () {
+
+        $(this).addClass('transition');
+    }, function () {
+
+        $(this).removeClass('transition');
+    });
+
+
+
+  
+
+
+
+
+});
